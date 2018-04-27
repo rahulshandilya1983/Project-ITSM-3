0ed300021d65d4a74992bb7273449e5bf2a6a41e
rahul <rahul@192.168.0.11>
2018-04-27 02:40:45 -0400

    Testing speak

--- __init__.py
+++ __init__.py
@@ -14,13 +14,14 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Mycroft Core.  If not, see <http://www.gnu.org/licenses/>.
-from os.path import dirname
+import re
+from os.path import dirname, join
 
 from adapt.intent import IntentBuilder
+from mycroft import MycroftSkill, intent_handler
 
 from mycroft.skills.core import MycroftSkill
 from mycroft.util.log import getLogger
-
 
 import requests
 
@@ -29,40 +30,22 @@ __author__ = 'Rahul Kumar Shandilya'
 LOGGER = getLogger(__name__)
 
 
-class ItsmUserSkill(MycroftSkill):
-    def __init__(self):
-        super(ItsmUserSkill, self).__init__(name="ItsmUserSkill")
-
-    def initialize(self):
-        self.load_data_files(dirname(__file__))
-        
-        
-        itsm_user_intent = IntentBuilder("itsmuserintent"). \
-            require("ItsmUserKeyword").build()
-        self.register_intent(itsm_user_intent, self.handle_itsm_user_intent)
-
-    def handle_itsm_user_intent(self, message):
-        url = 'https://dev22921.service-now.com/api/now/table/incident?sysparm_query=assigned_to%3D66e1f49edb5d13006b72712ebf9619c2%5Eactive%3Dtrue'
-        user = '531834'
-        pwd = 'Welcome!2345'
-        headers = {"Content-Type":"application/json","Accept":"application/json"}
-        # Do the HTTP request
-        response = requests.get(url, auth=(user, pwd), headers=headers )
-        # Check for HTTP codes other than 200
-        if response.status_code != 200: 
-            print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:',response.json())
-            exit()
-        # Decode the JSON response into a dictionary and use the data
-        data = response.json()
-        number = len(data['result'])
-        #self.speak("You have a total of {}".format(number)+"Incidents assigned")
-        self.speak("Give me details about Incident")
-        self.speak(message)
-        
+class IncidentDetailSkill(MycroftSkill):
+    @intent_handler(IntentBuilder("").require("Incident").require("Details"))
+    def speak_back(self, message):
+        """
+            Repeat the utterance back to the user.
+            TODO: The method is very english centric and will need
+                  localization.
+        """
+        # Remove everything up to the speak keyword and repeat that
+        utterance = message.data.get('utterance')
+        repeat = re.sub('^.*?' + message.data['Speak'], '', utterance)
+        self.speak(repeat.strip())
 
     def stop(self):
         pass
 
 
 def create_skill():
-    return ItsmUserSkill()
+    return IncidentDetailSkill()
